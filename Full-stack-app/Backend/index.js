297e263010beb550feba7b8c84899c2c97c1453e
Tejaswini Wakte <tejaswiniprasadwakte@gmail.com>
2025-01-20 12:58:21 +0530

    Update index.js

--- Full-stack-app/Backend/index.js
+++ Full-stack-app/Backend/index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
 
+app.get('/api', (req, res) => {
+  res.send('Hello from backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
